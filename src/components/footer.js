e81ecf7451b3190843fd389f6e186b1909680c18
Gyu-me <qkrrbsk03@gamil.com>
2026-04-01 18:41:48 +0900

    feat : 헤더 및 푸터 기본 구조 및 스타일 작성 - 헤더, 푸터 레이아웃 구성 - 기본 스타일 적용

---
+++ src/components/footer.js
@@ -0,0 +1,9 @@
+export function Footer() {
+  return `
+    <footer class="footer">
+      <div class="footer-inner">
+        <p>© 2026 포켓아카이브. All rights reserved.</p>
+      </div>
+    </footer>
+  `;
+}
--- src/main.js
+++ src/main.js
@@ -1,4 +1,109 @@
-// src/main.js
-import './style.css';
+import { Header } from "./components/header.js";
+import { Footer } from "./components/footer.js";
 
-console.log('포켓아카이브 실행 중!'); // 연결 확인용
+console.log("포켓아카이브 실행중");
+
+const app = document.getElementById("app");
+
+app.innerHTML = `
+  ${Header()}
+  <main class="main" id="content"></main>
+  ${Footer()}
+`;
+
+const menuBtn = document.getElementById("menuBtn");
+const sidebar = document.getElementById("sidebar");
+const overlay = document.getElementById("overlay");
+
+if (menuBtn) {
+  menuBtn.addEventListener("click", () => {
+    sidebar.classList.add("active");
+    overlay.classList.add("active");
+  });
+}
+
+async function loadPage() {
+  try {
+    const path = window.location.pathname;
+    let page = "./pages/pokedex.html";
+    let current = "home";
+
+    if (path.includes("myparty")) {
+      page = "./pages/myparty.html";
+      current = "myparty";
+    }
+    if (path.includes("board")) {
+      page = "./pages/board.html";
+      current = "board";
+    }
+    if (path.includes("mypage")) {
+      page = "./pages/mypage.html";
+      current = "mypage";
+    }
+
+    const res = await fetch(page);
+    const html = await res.text();
+
+    document.getElementById("content").innerHTML = html;
+
+    setActiveMenu(current);
+
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+loadPage();
+
+function setActiveMenu(current) {
+  //네비게이션 페이지 활성화
+ const navLinks = document.querySelectorAll(".nav a");
+
+  navLinks.forEach(link => {
+    link.classList.remove("active");
+    if (link.dataset.page === current) {
+      link.classList.add("active");
+    }
+  });
+
+  //사이드바 페이지 활성화
+  const links = document.querySelectorAll(".sidebar-nav a");
+
+  links.forEach(link => {
+    link.classList.remove("active");
+
+    if (link.dataset.page === current) {
+      link.classList.add("active");
+    }
+  });
+}
+
+//햄버거 버튼 활성화 시 사이드 메뉴바 활성화
+function initSidebar() {
+  const menuBtn = document.getElementById("menuBtn");
+  const sidebar = document.getElementById("sidebar");
+  const overlay = document.getElementById("overlay");
+  const closeBtn = document.getElementById("closeBtn");
+
+  if (!menuBtn || !sidebar || !overlay) return;
+
+  // 열기
+  menuBtn.addEventListener("click", () => {
+    sidebar.classList.add("active");
+    overlay.classList.add("active");
+  });
+
+  // 닫기 버튼
+  closeBtn?.addEventListener("click", () => {
+    sidebar.classList.remove("active");
+    overlay.classList.remove("active");
+  });
+
+  // 배경 클릭
+  overlay.addEventListener("click", () => {
+    sidebar.classList.remove("active");
+    overlay.classList.remove("active");
+  });
+}
+
+initSidebar();
