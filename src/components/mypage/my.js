5cca86a2be1943dd859e3fc096a3a36399794eec
djsy01 <dlsgh8884@naver.com>
2026-04-06 18:01:43 +0900

    feat: 마이페이지 로그아웃 버튼만 활성화

--- src/components/mypage/my.js
+++ src/components/mypage/my.js
@@ -34,6 +34,7 @@ export function init() {
 
   btnLogout.addEventListener('click', () => {
     if (confirm('로그아웃 하시겠습니까?')) {
+      localStorage.removeItem('token');
       window.location.href = '/';
     }
   });
