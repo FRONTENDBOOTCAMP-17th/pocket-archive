e843c4b1e36db67f9a42ea0e577ed975284e71e4
Gyu-me <qkrrbsk03@gamil.com>
2026-04-07 15:07:50 +0900

    fix : 멀티페이지설정

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,18 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        board: './pages/board.html',
+        detailPost: './pages/detailPost.html',
+        mypage: './pages/mypage.html',
+        myparty: './pages/myparty.html',
+        pokedex: './pages/pokedex.html',
+      }
+    }
+  },
   server: {
     proxy: {
       '/api': {
